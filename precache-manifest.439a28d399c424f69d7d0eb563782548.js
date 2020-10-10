e4b6ef2cca353d3e60045584218ef5f4b0219684
Andrzej <aserafin4@wp.pl>
2020-10-10 12:19:29 +0200

    Updates

---
+++ precache-manifest.439a28d399c424f69d7d0eb563782548.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa9b7f78bcbf9038db2d52c7600f1cad",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "56ecbe51072cbcd9c007",
+    "url": "/shop/static/css/main.0c739e7f.chunk.css"
+  },
+  {
+    "revision": "d1791d88d880a061ce7a",
+    "url": "/shop/static/js/2.dfc2cf47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shop/static/js/2.dfc2cf47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56ecbe51072cbcd9c007",
+    "url": "/shop/static/js/main.8c9874f4.chunk.js"
+  },
+  {
+    "revision": "df9297250f1712c7c654",
+    "url": "/shop/static/js/runtime-main.94850ce0.js"
+  },
+  {
+    "revision": "0841af952c807bdf56455b1addb4c7df",
+    "url": "/shop/static/media/IndieFlower-Regular.0841af95.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/shop/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "2c3f876fb5708059870dfa715014a1b4",
+    "url": "/shop/static/media/santa.2c3f876f.svg"
+  }
+]);
